3bdb198421fe3477e85496e98fdcbd8dcc963928
therexone <renerjake@gmail.com>
2020-05-16 06:24:31 +0530

    Add: Multiple streams Add: Stream select buttons Fix: flex css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ReactPlayer from 'react-player'
 import './App.css';
-import { config } from '../config';
+import { config } from './config';
+import { radioStreams } from './radioStreams';
 
 function App() {
 
   const [src, setSrc] = useState('./static/1.gif')
   const [urls, setUrls] = useState([])
+  const [streamLink, setStreamLink] = useState(radioStreams[0].link)
 
   const urlsRef = useRef();
   urlsRef.current = urls;
 
   useEffect(() => {
-    if (urlsRef.current.length === 0) {
+    const urls = JSON.parse(localStorage.getItem('urls'))
+    if (urls) {
+      setUrls(urls)
+      // console.log('got localdata')
+    }
+  }, [])
+
+  useEffect(() => {
+    if (urlsRef.length === 0) {
       const getGifSrc = async () => {
         let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${config.API_KEY}&q=synthwave&limit=50&offset=0&rating=G&lang=en`)
         let data = await response.json()
@@ -22,19 +32,11 @@ function App() {
         return urls
       }
       getGifSrc().then((urls) => {
-        console.log(urls);
         setUrls(urls)
         localStorage.setItem('urls', JSON.stringify(urls))
       });
     }
-  }, [])
-
-  useEffect(() => {
-    const urls = JSON.parse(localStorage.getItem('urls'))
-    if (urls) {
-      setUrls(urls)
-    }
-  }, [])
+  }, [urls.length])
 
 
   useEffect(() => {
@@ -43,14 +45,18 @@ function App() {
       setSrc(urlsRef.current[randInt])
       console.log(urlsRef.current[randInt])
     }
-      , 120000);
+      , 30000);
   }, [])
+
 
   return (
     <div className="gif" style={{ backgroundImage: `url(${src})` }}>
-      <div className="title">
-        <h1>【ｒａｄｉｏｗａｖｅ】</h1>
-        <ReactPlayer controls={true} id="player" playing url='http://air.radiorecord.ru:805/synth_320' height="40px" width="100%"/>
+      <h1>【ｒａｄｉｏｗａｖｅ】</h1>
+      <ReactPlayer controls={true} id="player" playing url={streamLink} height="40px" width="360px" />
+      <div className="stream-btns">
+        {radioStreams.map((stream, i) => (
+          <button key={i} className={`btn ${streamLink === stream.link && 'active'}`} onClick={() => setStreamLink(stream.link)}>{stream.name}</button>
+        ))}
 
       </div>
     </div>
---
+++ src/radioStreams.js
@@ -0,0 +1,20 @@
+export const radioStreams = [
+
+    {
+        name: 'Radiorecord.ru',
+        link: 'http://air.radiorecord.ru:805/synth_320'
+    },
+    {
+        name: 'Nightride.fm',
+        link: 'https://stream.nightride.fm/nightride.m4a'
+    },
+    {
+        name: 'Synthwave.hu',
+        link: 'http://139.162.14.151:9090/160mp3/;'
+    },
+    {
+        name: 'Artefaktor',
+        link: 'http://192.95.18.39:5288/stream'
+    }
+
+]
