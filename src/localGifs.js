38c78e5b1148b0a97fd2b63bb629a86046c355f7
therexone <renerjake@gmail.com>
2020-10-27 13:24:31 +0530

    fix: remove unused gif

--- src/localGifs.js
+++ src/localGifs.js
@@ -1,7 +1,7 @@
 export const localGifs = [
-    'https://media2.giphy.com/media/l4Jzi0iyKHLe1qT9S/giphy.gif?cid=6ee29fd7360f728580c0968cdd1a6652cf795ba6a4f541d1&rid=giphy.gif',
-    './static/1.gif',
-    './static/2.gif',
-    './static/4.gif'
-
-]
+  "https://media2.giphy.com/media/l4Jzi0iyKHLe1qT9S/giphy.gif?cid=6ee29fd7360f728580c0968cdd1a6652cf795ba6a4f541d1&rid=giphy.gif",
+  "./static/1.gif",
+  "./static/2.gif",
+  "./static/4.gif",
+  "./static/3.gif",
+];
