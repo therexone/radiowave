878363eb29e75e2b0902bcb981c9fe0d0127d9d0
therexone <renerjake@gmail.com>
2020-10-28 18:22:06 +0530

    Updates

---
+++ precache-manifest.67f6b83298578fe733e72fdae8bc3079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf8810d4b56a00f478444d49f268d31",
+    "url": "/radiowave/index.html"
+  },
+  {
+    "revision": "3b63b54d0d776616cf55",
+    "url": "/radiowave/static/css/main.30c73bcc.chunk.css"
+  },
+  {
+    "revision": "3b473dc9384ce5429981",
+    "url": "/radiowave/static/js/2.180baed4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/radiowave/static/js/2.180baed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b63b54d0d776616cf55",
+    "url": "/radiowave/static/js/main.7fb50c62.chunk.js"
+  },
+  {
+    "revision": "7978c5b7c7e76910e2ed",
+    "url": "/radiowave/static/js/runtime-main.39c507b7.js"
+  }
+]);
