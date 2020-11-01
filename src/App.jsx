76b75e3bdc35eb37cfaee4cc7c3bee046e9dd114
therexone <renerjake@gmail.com>
2020-11-01 06:33:10 +0530

    fix: missing config, rename to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 
 import "./App.css";
-import { config } from "./config";
+import { API_KEY} from "./config";
 import { radioStreams } from "./radioStreams";
 import { localGifs } from "./localGifs";
 
@@ -38,7 +38,7 @@ function App() {
     if (urlsRef.current.length === 0) {
       const getGifSrc = async () => {
         let response = await fetch(
-          `https://api.giphy.com/v1/gifs/search?api_key=${config.API_KEY}&q=synthwave&limit=50&offset=0&rating=G&lang=en`
+          `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=synthwave&limit=50&offset=0&rating=G&lang=en`
         );
         let data = await response.json();
         let srcList = data.data;
